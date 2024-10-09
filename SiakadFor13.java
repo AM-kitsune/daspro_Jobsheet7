d59ceb4e40441e89668c32cc95af17553af870a2
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2024-10-10 00:30:59 +0700

    Update SiakadFor13.java

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 
 public class SiakadFor13 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc13 = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+        int batasKelulusan = 60; 
 
-        for (int i = 1; i <= 10; i++){
-            System.out.print("Masukan nilai Mahasiswa ke- " + i + ": ");
-            nilai =  sc13.nextDouble();
-            if (nilai > tertinggi ) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc13.nextDouble();
+
+            if (nilai > tertinggi) {
                 tertinggi = nilai;
-            }else if(nilai < terendah){
+            }
+
+            if (nilai < terendah) {
                 terendah = nilai;
+            }
+
+            if (nilai >= batasKelulusan) {
+                lulus++; 
+            } else {
+                tidakLulus++; 
             }
         }
 
-        System.out.println("Nilai Tertinggi: "+ tertinggi);
-        System.out.println("Nilai Tertinggi: "+ terendah);
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
+
     }
 }
